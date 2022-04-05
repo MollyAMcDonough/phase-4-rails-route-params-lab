a8a331962c99dd17c14d32ae0e172e43b837a4e0
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-04-05 18:38:57 -0400

    commit tree dc282c74024a2c34dfe1b15df7b0b5718292e96d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
+  def name_search
+    byebug
+  end
 
   def index
-    students = Student.all
+    if params.include?(:name)
+      students = Student.where("lower(first_name) = ? OR lower(last_name) = ?", params[:name].downcase, params[:name].downcase)
+    else
+      students = Student.all
+    end
     render json: students
   end
 
+  def show
+    render json: Student.find(params[:id])
+  end
+
+  
 end
